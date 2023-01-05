0f812043722ba604af7584ccdf77207f675b0f39
qyzzs <qyzzzs@163.com>
2023-01-05 21:05:02 +0800

    add ant design vue

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import router from './router'
+import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+app.use(Antd);
+app.mount("#app");
